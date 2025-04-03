b088f1a464e79d73e97822e69e1e8d25554c4ace
Michael Köck <mkoeck@elektroshopkoeck.com>
2025-04-03 16:48:42 +0200

    [MIG] vault: use correct super

--- vault/static/src/backend/list_renderer.esm.js
+++ vault/static/src/backend/list_renderer.esm.js
@@ -5,8 +5,7 @@ import {patch} from "@web/core/utils/patch";
 
 patch(ListRenderer.prototype, {
     getCellTitle(column) {
-        const _super = this._super.bind(this);
         const attrs = column.rawAttrs || {};
-        if (attrs.widget !== "vault_field") return _super(...arguments);
+        if (attrs.widget !== "vault_field") return super.getCellTitle(...arguments);
     },
 });
